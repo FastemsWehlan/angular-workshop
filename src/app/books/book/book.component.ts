b81f2787bb8f093279ccbf67512b693cf85f0c88
Wehlan, Thomas <thomas.wehlan@fastems.com>
2019-03-27 09:30:08 +0100

    Tag 1 (inkl. Übungen)

---
+++ src/app/books/book/book.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss']
+})
+export class BookComponent implements OnInit {
+
+  @Input()
+  book: Book;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  get stars() {
+   return new Array(this.book.rating);
+  }
+}
---
+++ src/app/books/dashboard/dashboard.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/Book';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  books: Book[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+
+    this.books = [{
+      isbn: '111',
+      title: 'AngularJS',
+      description: 'Tolles Buch!',
+      rating: 3
+    }, {
+      isbn: '222',
+      title: 'Angular',
+      description: 'Super Buch!',
+      rating: 5
+    }, {
+      isbn: '333',
+      title: 'React',
+      description: 'Geht so!',
+      rating: 1
+    }, {
+      isbn: '444',
+      title: 'Keine Angst vor Unix',
+      description: 'Standardwerk!',
+      rating: 4
+    }, {
+      isbn: '555',
+      title: 'Powershell',
+      description: 'Braucht man nicht!',
+      rating: 0
+    }];
+  }
+}
