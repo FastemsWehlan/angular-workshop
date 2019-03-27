bce697176349f3d1a4724f6c34db20028aed8009
Wehlan, Thomas <thomas.wehlan@fastems.com>
2019-03-27 16:58:45 +0100

    Unit test

--- src/app/books/book/book.component.spec.ts
+++ src/app/books/book/book.component.spec.ts
@@ -6,6 +6,10 @@ describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
+  ///  Angular Magic....
+  ///  async will first compileComponents
+  ///  then the next beforeEach
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ BookComponent ]
@@ -16,6 +20,12 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+    component.book = {
+      isbn: '000',
+      title: 'TEST',
+      description: '...',
+      rating: 0
+    };
     fixture.detectChanges();
   });
 
