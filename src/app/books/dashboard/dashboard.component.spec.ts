6ca9dca045b0cdb41c0e6f14090f06f060807073
Wehlan, Thomas <thomas.wehlan@fastems.com>
2019-03-27 17:28:47 +0100

    poor mans behavior testing

--- src/app/books/dashboard/dashboard.component.spec.ts
+++ src/app/books/dashboard/dashboard.component.spec.ts
@@ -2,28 +2,49 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
 import { BookComponent } from '../book/book.component';
+import { BookRatingService } from '../shared/book-rating.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
+  let rateUpWasCalled = false;
+
   beforeEach(async(() => {
+
+    const bookRatingMock = {
+      rateUp: (book) => { rateUpWasCalled = true; return book; },
+      rateDown: (book) => { return book; }
+    };
+
     TestBed.configureTestingModule({
       declarations: [
         DashboardComponent,
         BookComponent  // Integration Test
+      ],
+      providers: [
+        {
+          provide: BookRatingService,
+          useValue: bookRatingMock
+        }
       ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('doRateUp() should forward the execution to BookRatingService', () => {
+
+    let testBook = {
+      isbn: '000',
+      title: 'TEST',
+      description: '...',
+      rating: 0
+    };
+
+    component.doRateUp(testBook);
+    expect(rateUpWasCalled).toBe(true);
   });
 });
