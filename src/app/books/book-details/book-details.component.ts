0480be0f8d0917e59e4cce280011b2f44dccb885
Thomas Wehlan <thomas.wehlan@fastems.com>
2019-03-29 14:07:36 +0100

    Fix: Anti-Pattern #2

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BookStoreService } from '../shared/book-store.service';
 import { Book } from '../shared/Book';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map, switchMap, share } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, switchMap, share, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'br-book-details',
@@ -22,7 +22,17 @@ export class BookDetailsComponent implements OnInit {
     this.book$ = this.route.paramMap
       .pipe(
         map(params => params.get('isbn')),
-        switchMap(isbn => this.store.getSingle(isbn)),
-      );
+        switchMap(isbn => this.store.getSingle(isbn)
+          .pipe(catchError(e => of({
+            isbn: '???',
+            title: e.url,
+            description: 'Sorry, Justus, Peter und Bob sind gerade nicht in Rocky Beach.',
+            rating: -1,
+            price: 0.00,
+            firstThumbnailUrl: ''
+          })),
+        )
+      )
+    );
   }
 }
