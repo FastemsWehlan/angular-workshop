0e47be7dee299d90eaf3c1753b0e98489c2f2e9a
Thomas Wehlan <thomas.wehlan@fastems.com>
2019-03-29 12:09:55 +0100

    Anti-pattern: subscribing two times async

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BookStoreService } from '../shared/book-store.service';
 import { Book } from '../shared/Book';
 import { ActivatedRoute } from '@angular/router';
-import { of, Observable, Subscriber } from 'rxjs';
-import { map, filter, scan } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map, switchMap, share } from 'rxjs/operators';
 
 @Component({
   selector: 'br-book-details',
@@ -19,44 +19,11 @@ export class BookDetailsComponent implements OnInit {
      private route: ActivatedRoute) { }
 
   ngOnInit() {
-
-    this.isbn$ = this.route.paramMap
+    this.book$ = this.route.paramMap
       .pipe(
-        map(params => params.get('isbn'))
-        );
-
-    // --------
-
-    const observer = {
-      next: zahl => console.log(zahl),
-      error: e => console.log('ERROR', e),
-      complete: () => console.log('Complete!')
-    };
-
-    const observable$ = new Observable<number>(subscriber => {
-      subscriber.next(2);
-      subscriber.next(4);
-      subscriber.next(6);
-      subscriber.complete();
-      window.setTimeout(() => subscriber.next(99), 2000);
-    });
-
-    const subscription = observable$
-      .pipe(
-        map(x => x * 10),       // 20, 40, 60
-        filter(x => x > 20),    // 40, 60
-        scan((a,b) => a + b)    // 100, 40
-      )
-      .subscribe(observer);
-
-    window.setTimeout(() => subscription.unsubscribe(), 2000);
-
-    // subscription.unsubscribe();
-
-    // // creation funtions
-    // of(1, 2, 3).subscribe(
-    //   zahl => console.log(zahl), e => console.log('ERROR', e),
-    //   () => console.log('Complete!')
-    //   );
+        map(params => params.get('isbn')),
+        switchMap(isbn => this.store.getSingle(isbn)),
+        share()
+      );
   }
 }
